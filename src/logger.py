3c35b11be5b7863725e8bd7cf8563b9d0081c42b
samarthjoshi2018--a11y <samarthjoshi2018@gmail.com>
2025-08-01 15:51:06 +0530

    EDA and problem statement

--- src/logger.py
+++ src/logger.py
@@ -12,6 +12,3 @@ logging.basicConfig(
     format="['%(asctime)s ]- %(levelname)s - %(name)s - %(message)s",
     level=logging.INFO,
 )
-
-if __name__=="__main__":
-    logging.info("logging has started")
